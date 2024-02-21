4b7ef2bb3fba2a660537bf10d229898010fbd687
Chris <chrisdniels12@gmail.com>
2024-02-21 14:59:42 -0800

    add index and js folder content
    added finished assignment

---
+++ js/script.js
@@ -0,0 +1,152 @@
+// STEP 1
+
+// let someMonth;
+// function theMonth() {};
+// let currentMonth;
+// let summerMonth;
+// let myLibraryFunction; 
+
+// STEP 2
+
+// 2   // numeric literal
+// "hello world"   // string literal
+// true   // boolean literal 
+// null // literal null value
+
+// STEP 3
+
+// let x = 6 + 22;   // complex expression
+// let y = x + 0;    // complex expression
+
+// STEP 4
+
+// let strFirstName;
+// let strLastName;
+// let strAddress;
+// let strCity;
+// let strState;
+// let intZipCode;
+// let yourAge;
+// let referralSource;
+// let mayWeContactYou;
+
+// STEP 5
+
+// { let strFirstName;
+// strFirstName = "Chris";    // declare and assign on seperate lines
+// }
+// const yourAge = 26;        // declare and assign on the same line
+
+// let mayWeContactYou = true, strFirstName = "John"    // asign multiple variables on the same line
+
+// STEP 6
+
+// let welcome;
+// welcome = "hello";
+// console.log(welcome + 123);
+
+// STEP 7
+
+// let food = "bananas";
+// console.log(food + true);
+
+// let x = 25;
+// console.log(x + false);
+
+// STEP 8
+
+// not this string is was not valid, in order to fix it, add an escape sequence before the alone single quote, see below
+// let someString = 'Who once said, "Only two things are infinite, the universe and human stupidity, and I\'m not sure about the former."'
+// console.log(someString)
+
+// STEP 9
+
+// let empty = null;
+// console.log(empty);
+// let notDefined = undefined;
+// console.log(notDefined);
+
+// STEP 10
+
+// console.log(typeof(""))
+// let g = 6;
+// g++;
+// console.log(g);
+// console.log( !(g != 6));
+// let k = {two: 2, three: 3};
+// delete k.three;
+// console.log(k);
+// k = void {two: 2};
+// console.log(k)
+
+// STEP 11
+
+// window.alert("Hello " + "Chris Daniels" + ", welcome to the JavaScript class!")
+
+// STEP 12
+
+// let name = "Chris Daniels";
+// window.alert("Hello " + name + ", welcome to the JavaScript class!");
+
+// STEP 13
+
+// let course = "JavaScript";
+// name = "Chris Daniels";
+// window.alert("Hello " + name + ", welcome to the " + course + " class!");
+
+// STEP 14
+
+// course = "JavaScript";
+// name = "Chris Daniels";
+// window.alert("Hello " + name + "." + "\nWelcome to the " + course + " class!");
+
+// STEP 15
+
+// name = prompt("What's your name?");
+
+// STEP 16
+
+// course = prompt("What course are you taking?");
+
+// STEP 17
+
+// let x = 10;
+// let y = 20;
+// console.log(x+y)
+ 
+// STEP 18
+
+// let x = 20;
+// x += 20;
+// console.log(x);
+
+// STEP 19 
+
+// let x = 20;
+// x *= 5;
+// console.log(x);
+
+// STEP 20
+
+// let x = 20 % 3;
+// x /= 1;
+// console.log(x);
+
+//  STEP 21
+
+// let a = 2;
+// let b = 4;
+// if ( a < b && typeof(b) === "number" ) {
+//     console.log(true)
+// }
+
+// STEP 22
+
+// let r = "reno"
+// let v = "vegas"
+// if (r == v || v == "reno") {
+//     console.log(true)
+// } else {
+//     console.log(false)
+// }
+
